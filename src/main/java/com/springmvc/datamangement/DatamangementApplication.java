ef838a6fa9e79d670134297c89798ff585741a8d
SIVA1278 <sivaperumal1278@gmail.com>
2026-03-27 22:53:39 +0530

    add file

---
+++ src/main/java/com/springmvc/datamangement/DatamangementApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.datamangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamangementApplication.class, args);
+	}
+
+}
